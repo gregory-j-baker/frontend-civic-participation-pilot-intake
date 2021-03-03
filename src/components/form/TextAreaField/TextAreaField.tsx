148ee52ca8e434d8798033ffdaf2a6b7b4b70ff2
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2021-03-03 15:48:32 -0400

    Added TextAreaField.

--- src/components/form/TextAreaField/TextAreaField.tsx
+++ src/components/form/TextAreaField/TextAreaField.tsx
@@ -4,11 +4,11 @@ import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 
-export interface ITextFieldOnChangeEvent {
+export interface ITextAreaFieldOnChangeEvent {
   (event: { field: string; value: string | null }): void;
 }
 
-export interface ITextFieldProps {
+export interface ITextAreaFieldProps {
   className?: string;
   disabled?: boolean;
   error?: boolean;
@@ -16,14 +16,14 @@ export interface ITextFieldProps {
   gutterBottom?: boolean;
   helperText?: string;
   label: string;
-  onChange: ITextFieldOnChangeEvent;
-  maxLength?: number;
+  onChange: ITextAreaFieldOnChangeEvent;
   placeholder?: string;
   required?: boolean;
+  rows?: number;
   value?: string | null;
 }
 
-const TextField = ({ className, disabled, error, field, gutterBottom, helperText, label, onChange, maxLength, placeholder, required, value }: ITextFieldProps): JSX.Element => {
+const TextAreaField = ({ className, disabled, error, field, gutterBottom, helperText, label, onChange, placeholder, required, rows, value }: ITextAreaFieldProps): JSX.Element => {
   const { t } = useTranslation();
 
   const fieldId = `form-text-field-${field}`;
@@ -34,15 +34,15 @@ const TextField = ({ className, disabled, error, field, gutterBottom, helperText
   };
 
   return (
-    <FormGroup controlId={fieldId} validationState={error ? 'error' : null} className={gutterBottom ? 'tw-mb-8' : 'tw-mb-0'}>
+    <FormGroup controlId={fieldId} validationState={error ? 'error' : null} className={gutterBottom ? 'tw-mb-8' : 'tw-mb-0'} bsSize="small">
       <ControlLabel className={`${required ? 'required' : null}`}>
         <span className="field-name">{label}</span>
         {required && <strong className="required tw-ml-1">{t('common:field-required')}</strong>}
       </ControlLabel>
-      <FormControl type="text" id={fieldId} value={value ?? ''} disabled={disabled} onChange={handleOnChange} maxLength={maxLength} placeholder={placeholder} className={className} />
+      <FormControl componentClass="textarea" id={fieldId} value={value ?? ''} disabled={disabled} onChange={handleOnChange} placeholder={placeholder} className={className} rows={rows ?? 5} />
       {helperText && <HelpBlock>{helperText}</HelpBlock>}
     </FormGroup>
   );
 };
 
-export default TextField;
+export default TextAreaField;
