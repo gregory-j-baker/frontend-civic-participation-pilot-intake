5c9d23a6be0741bc92c506fe41dd10e012ad5883
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2021-03-15 08:48:06 -0300

    Fixed 404 style.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,52 +8,56 @@
 import useTranslation from 'next-translate/useTranslation';
 import { MainLayout } from '../components/layouts/main/MainLayout';
 
+interface ListItemProps {
+  children: React.ReactNode;
+}
+
+const ListItem = ({ children }: ListItemProps): JSX.Element => <li className="tw-my-1">{children}</li>;
+
 const Custom404 = (): JSX.Element => {
   const { t, lang } = useTranslation();
   return (
     <MainLayout showBreadcrumb={false}>
-      <div className="mwstext section">
-        <div className="row mrgn-tp-lg">
-          <div className="col-xs-3 col-sm-2 col-md-1 text-center mrgn-tp-md">
-            <span className="glyphicon glyphicon-warning-sign glyphicon-error"></span>
+      <div className="tw-my-8">
+        <div className="tw-flex">
+          <div className="tw-w-24 tw-flex-shrink-0">
+            <span className="glyphicon glyphicon-warning-sign glyphicon-error tw-flex-shrink-0"></span>
           </div>
-          <div className="col-xs-9 col-sm-10 col-md-11">
-            <h1 className="mrgn-tp-md">{t('common:custom-404.header')}</h1>
-            <p className="pagetag">
+          <div className="tw-flex-grow">
+            <h1 className="tw-mt-0">{t('common:custom-404.header')}</h1>
+            <p className="tw-m-0">
               <b>{t('common:custom-404.error-code')}</b>
             </p>
           </div>
         </div>
-        <div className="row mrgn-bttm-lg">
-          <div className="col-md-12">
-            <p>{t('common:custom-404.description')}</p>
-            {lang === 'fr' ? (
-              <ul>
-                <li>
-                  Retournez à la <a href="https://www.canada.ca/fr.html">page d’accueil</a>;
-                </li>
-                <li>
-                  Consultez le <a href="https://www.canada.ca/fr/plan.html">plan du site</a>;
-                </li>
-                <li>
-                  <a href="https://www.canada.ca/fr/contact.html">Communiquez avec nous</a> pour obtenir de l’aide.
-                </li>
-              </ul>
-            ) : (
-              <ul>
-                <li>
-                  Return to the <a href="https://www.canada.ca/en.html">home page</a>;
-                </li>
-                <li>
-                  Consult the <a href="https://www.canada.ca/en/sitemap.html">site map</a>; or
-                </li>
-                <li>
-                  <a href="https://www.canada.ca/en/contact.html">Contact us</a> and we&apos;ll help you out.
-                </li>
-              </ul>
-            )}
-          </div>
-        </div>
+
+        <p className="tw-m-0 tw-my-4">{t('common:custom-404.description')}</p>
+
+        {lang === 'fr' ? (
+          <ul className="tw-list-disc tw-list-inside">
+            <ListItem>
+              Retournez à la <a href="https://www.canada.ca/fr.html">page d’accueil</a>;
+            </ListItem>
+            <ListItem>
+              Consultez le <a href="https://www.canada.ca/fr/plan.html">plan du site</a>;
+            </ListItem>
+            <ListItem>
+              <a href="https://www.canada.ca/fr/contact.html">Communiquez avec nous</a> pour obtenir de l’aide.
+            </ListItem>
+          </ul>
+        ) : (
+          <ul>
+            <ListItem>
+              Return to the <a href="https://www.canada.ca/en.html">home page</a>;
+            </ListItem>
+            <ListItem>
+              Consult the <a href="https://www.canada.ca/en/sitemap.html">site map</a>; or
+            </ListItem>
+            <ListItem>
+              <a href="https://www.canada.ca/en/contact.html">Contact us</a> and we&apos;ll help you out.
+            </ListItem>
+          </ul>
+        )}
       </div>
     </MainLayout>
   );
