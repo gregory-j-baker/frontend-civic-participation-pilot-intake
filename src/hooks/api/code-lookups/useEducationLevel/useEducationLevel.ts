6dec14d0dcb86171d317a271aa1ebd9da9b88af4
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2021-03-22 11:59:29 -0300

    Added get single resource hooks.

---
+++ src/hooks/api/code-lookups/useEducationLevel/useEducationLevel.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2021 Her Majesty the Queen in Right of Canada, as represented by the Employment and Social Development Canada
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { QueryFunctionContext, UseQueryResult } from 'react-query';
+import { useQuery, QueryFunction } from 'react-query';
+import { HttpClientResponseError } from '../../../../common/HttpClientResponseError';
+import { fetchWrapper } from '../../../../utils/fetch-wrapper';
+import { EducationLevel, educationLevelsQueryKey, educationLevelsUri } from '../types';
+
+export const fetchEducationLevel: QueryFunction<Promise<EducationLevel>> = ({ queryKey }: QueryFunctionContext) => {
+  const educationLevelId = queryKey[1] as string;
+  return fetchWrapper<EducationLevel>(`${educationLevelsUri}/${educationLevelId}`);
+};
+
+export const useEducationLevel = (educationLevelId: string): UseQueryResult<EducationLevel, HttpClientResponseError> => {
+  return useQuery([educationLevelsQueryKey, educationLevelId], fetchEducationLevel, {
+    cacheTime: Infinity,
+    staleTime: Infinity,
+  });
+};
---
+++ src/hooks/api/code-lookups/useProvince/useProvince.tsx
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2021 Her Majesty the Queen in Right of Canada, as represented by the Employment and Social Development Canada
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { QueryFunctionContext, UseQueryResult } from 'react-query';
+import { useQuery, QueryFunction } from 'react-query';
+import { HttpClientResponseError } from '../../../../common/HttpClientResponseError';
+import { fetchWrapper } from '../../../../utils/fetch-wrapper';
+import { Province, provincesQueryKey, provincesUri } from '../types';
+
+export const fetchProvince: QueryFunction<Promise<Province>> = ({ queryKey }: QueryFunctionContext) => {
+  const provinceId = queryKey[1] as string;
+  return fetchWrapper<Province>(`${provincesUri}/${provinceId}`);
+};
+
+export const useProvince = (provinceId: string): UseQueryResult<Province, HttpClientResponseError> => {
+  return useQuery([provincesQueryKey, provinceId], fetchProvince, {
+    cacheTime: Infinity,
+    staleTime: Infinity,
+  });
+};
