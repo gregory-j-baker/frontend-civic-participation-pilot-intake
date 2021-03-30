7ecd0da33f47a64c095f2d1b06a30b3df292dce5
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2021-03-30 13:00:28 -0300

    Added mgm applications listing translation

--- src/pages/management/applications/index.tsx
+++ src/pages/management/applications/index.tsx
@@ -21,13 +21,14 @@ import Link from 'next/link';
 import { ApplicationStatus, Language, Province } from '../../../hooks/api/code-lookups/types';
 import { useApplicationStatuses } from '../../../hooks/api/code-lookups/useApplicationStatuses';
 import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
 
 interface RouterQuery {
   page?: number;
 }
 
 const ManagementApplicationsPage = (): JSX.Element => {
-  const { lang } = useTranslation();
+  const { t, lang } = useTranslation();
 
   const router = useRouter();
   const { page } = router.query as RouterQuery;
@@ -47,102 +48,110 @@ const ManagementApplicationsPage = (): JSX.Element => {
 
   return (
     <MainLayout>
-      <h1 className="tw-m-0 tw-mb-10 tw-border-none">Management - Applications Listing here!</h1>
       {isApplicationsLoading || isApplicationStatusesLoading || isLanguagesLoading || isProvincesLoading ? (
         <PageLoadingSpinner />
       ) : (
-        <div className="tw-flex tw-flex-col">
-          <div className="tw--my-2 tw-overflow-x-auto sm:tw--mx-6 lg:tw--mx-8">
-            <div className="tw-py-2 tw-align-middle tw-inline-block tw-min-w-full sm:tw-px-6 lg:tw-px-8">
-              <div className="tw-shadow tw-overflow-hidden tw-border-b tw-border-gray-200 sm:tw-rounded-lg">
-                <table className="tw-min-w-full tw-divide-y tw-divide-gray-200">
-                  <thead className="tw-bg-gray-50">
-                    <tr>
-                      <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
-                        Name
-                      </th>
-                      <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
-                        Language
-                      </th>
-                      <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
-                        Province
-                      </th>
-                      <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
-                        Date recived
-                      </th>
-                      <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
-                        Status
-                      </th>
-                      <th scope="col" className="tw-relative tw-px-4 tw-py-3">
-                        <span className="tw-sr-only">Edit</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  {applicationsResponse && (
-                    <tbody className="tw-bg-white tw-divide-y tw-divide-gray-200">
-                      {applicationsResponse._embedded.applications.length > 0 ? (
-                        applicationsResponse._embedded.applications.map((application) => (
-                          <tr key={application.id}>
-                            <td className="tw-px-4 tw-py-2 tw-whitespace-nowrap">
-                              <div className="tw-text-sm tw-font-bold tw-text-gray-900">{`${application.firstName} ${application.lastName}`}</div>
-                              <div className="tw-text-sm tw-text-gray-500">{`${application.email}`}</div>
-                            </td>
-                            <td className="tw-px-4 tw-py-2 tw-text-sm">{languages && getDescription(languages._embedded.languages.find((obj) => obj.id === application.languageId) as Language)}</td>
-                            <td className="tw-px-4 tw-py-2 tw-text-sm">{provinces && getDescription(provinces._embedded.provinces.find((obj) => obj.id === application.provinceId) as Province)}</td>
-                            <td className="tw-px-4 tw-py-2 tw-text-sm tw-whitespace-nowrap">{dateTimeFormat.format(new Date(application.createdDate))}</td>
-                            <td className="tw-px-4 tw-py-2 tw-text-sm">{applicationStatuses && getDescription(applicationStatuses._embedded.applicationStatuses.find((obj) => obj.id === application.applicationStatusId) as ApplicationStatus)}</td>
-                            <td className="tw-px-4 tw-py-2 tw-text-sm tw-whitespace-nowrap tw-text-right tw-font-bold">
-                              <Link href={`/management/applications/${application.id}`} passHref>
-                                <a className="tw-text-indigo-600 hover:tw-text-indigo-900">Edit</a>
-                              </Link>
+        <>
+          <NextSeo title={t('application:management.list.title')} />
+
+          <h1 id="wb-cont" className="tw-m-0 tw-border-none tw-mb-8 tw-text-3xl">
+            {t('common:app.title')}
+          </h1>
+          <h2 className="tw-m-0 tw-mb-10 tw-text-2xl">{t('application:management.list.header')}</h2>
+
+          <div className="tw-flex tw-flex-col">
+            <div className="tw--my-2 tw-overflow-x-auto sm:tw--mx-6 lg:tw--mx-8">
+              <div className="tw-py-2 tw-align-middle tw-inline-block tw-min-w-full sm:tw-px-6 lg:tw-px-8">
+                <div className="tw-shadow tw-overflow-hidden tw-border-b tw-border-gray-200 sm:tw-rounded-lg">
+                  <table className="tw-min-w-full tw-divide-y tw-divide-gray-200">
+                    <thead className="tw-bg-gray-50">
+                      <tr>
+                        <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
+                          {t('application:management.list.table-header.name')}
+                        </th>
+                        <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
+                          {t('application:management.list.table-header.language')}
+                        </th>
+                        <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
+                          {t('application:management.list.table-header.province')}
+                        </th>
+                        <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
+                          {t('application:management.list.table-header.date-received')}
+                        </th>
+                        <th scope="col" className="tw-px-4 tw-py-3 tw-text-left tw-text-xs tw-text-gray-500 tw-uppercase tw-tracking-wider">
+                          {t('application:management.list.table-header.status')}
+                        </th>
+                        <th scope="col" className="tw-relative tw-px-4 tw-py-3">
+                          <span className="tw-sr-only">{t('application:management.list.table-header.edit')}</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    {applicationsResponse && (
+                      <tbody className="tw-bg-white tw-divide-y tw-divide-gray-200">
+                        {applicationsResponse._embedded.applications.length > 0 ? (
+                          applicationsResponse._embedded.applications.map((application) => (
+                            <tr key={application.id}>
+                              <td className="tw-px-4 tw-py-2 tw-whitespace-nowrap">
+                                <div className="tw-text-sm tw-font-bold tw-text-gray-900">{`${application.firstName} ${application.lastName}`}</div>
+                                <div className="tw-text-sm tw-text-gray-500">{`${application.email}`}</div>
+                              </td>
+                              <td className="tw-px-4 tw-py-2 tw-text-sm">{languages && getDescription(languages._embedded.languages.find((obj) => obj.id === application.languageId) as Language)}</td>
+                              <td className="tw-px-4 tw-py-2 tw-text-sm">{provinces && getDescription(provinces._embedded.provinces.find((obj) => obj.id === application.provinceId) as Province)}</td>
+                              <td className="tw-px-4 tw-py-2 tw-text-sm tw-whitespace-nowrap">{dateTimeFormat.format(new Date(application.createdDate))}</td>
+                              <td className="tw-px-4 tw-py-2 tw-text-sm">{applicationStatuses && getDescription(applicationStatuses._embedded.applicationStatuses.find((obj) => obj.id === application.applicationStatusId) as ApplicationStatus)}</td>
+                              <td className="tw-px-4 tw-py-2 tw-text-sm tw-whitespace-nowrap tw-text-right tw-font-bold">
+                                <Link href={`/management/applications/${application.id}`} passHref>
+                                  <a className="tw-text-indigo-600 hover:tw-text-indigo-900">{t('application:management.list.edit-link')}</a>
+                                </Link>
+                              </td>
+                            </tr>
+                          ))
+                        ) : (
+                          <tr>
+                            <td className="tw-px-4 tw-py-2 tw-text-sm tw-whitespace-nowrap tw-text-center" colSpan={7}>
+                              {t('common:pagination.no-data')}
                             </td>
                           </tr>
-                        ))
-                      ) : (
-                        <tr>
-                          <td className="tw-px-4 tw-py-2 tw-text-sm tw-whitespace-nowrap tw-text-center" colSpan={7}>
-                            No matching entries found
-                          </td>
-                        </tr>
-                      )}
-                    </tbody>
-                  )}
-                </table>
-                {applicationsResponse?.page && (
-                  <div className="tw-bg-white tw-px-4 tw-py-3 tw-flex tw-items-center tw-justify-between tw-border-t tw-border-gray-200">
-                    <div className="tw-hidden sm:tw-block">
-                      <div className="tw-text-sm tw-text-gray-700 tw-hidden sm:tw-block">
-                        Showing&nbsp;
-                        <span className="tw-font-bold">{(applicationsResponse.page.number - 1) * applicationsResponse.page.size + 1}</span>
-                        &nbsp;to&nbsp;
-                        <span className="tw-font-bold">{applicationsResponse.page.number === applicationsResponse.page.totalPages ? applicationsResponse.page.totalElements : applicationsResponse.page.number * applicationsResponse.page.size}</span>
-                        &nbsp;of&nbsp;
-                        <span className="tw-font-bold">{applicationsResponse.page.totalElements}</span>
-                        &nbsp;entries
+                        )}
+                      </tbody>
+                    )}
+                  </table>
+                  {applicationsResponse?.page && (
+                    <div className="tw-bg-white tw-px-4 tw-py-3 tw-flex tw-items-center tw-justify-between tw-border-t tw-border-gray-200">
+                      <div className="tw-hidden sm:tw-block">
+                        <div className="tw-text-sm tw-text-gray-700 tw-hidden sm:tw-block">
+                          {t('common:pagination.info.showing')}&nbsp;
+                          <span className="tw-font-bold">{(applicationsResponse.page.number - 1) * applicationsResponse.page.size + 1}</span>
+                          &nbsp;{t('common:pagination.info.to')}&nbsp;
+                          <span className="tw-font-bold">{applicationsResponse.page.number === applicationsResponse.page.totalPages ? applicationsResponse.page.totalElements : applicationsResponse.page.number * applicationsResponse.page.size}</span>
+                          &nbsp;{t('common:pagination.info.of')}&nbsp;
+                          <span className="tw-font-bold">{applicationsResponse.page.totalElements}</span>
+                          &nbsp;{t('common:pagination.info.results')}
+                        </div>
+                      </div>
+                      <div className="tw-flex tw-items-center tw-justify-between tw-space-x-4">
+                        {applicationsResponse.page.number > 1 && (
+                          <Link href={`/management/applications?page=${applicationsResponse.page.number - 1}`} passHref>
+                            <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
+                              {t('common:pagination.previous')}
+                            </a>
+                          </Link>
+                        )}
+                        {applicationsResponse.page.number < applicationsResponse.page.totalPages && (
+                          <Link href={`/management/applications?page=${applicationsResponse.page.number + 1}`} passHref>
+                            <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
+                              {t('common:pagination.next')}
+                            </a>
+                          </Link>
+                        )}
                       </div>
                     </div>
-                    <div className="tw-flex tw-items-center tw-justify-between tw-space-x-4">
-                      {applicationsResponse.page.number > 1 && (
-                        <Link href={`/management/applications?page=${applicationsResponse.page.number - 1}`} passHref>
-                          <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
-                            Previous
-                          </a>
-                        </Link>
-                      )}
-                      {applicationsResponse.page.number < applicationsResponse.page.totalPages && (
-                        <Link href={`/management/applications?page=${applicationsResponse.page.number + 1}`} passHref>
-                          <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
-                            Next
-                          </a>
-                        </Link>
-                      )}
-                    </div>
-                  </div>
-                )}
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </MainLayout>
   );
