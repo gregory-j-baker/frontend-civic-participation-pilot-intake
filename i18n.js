7e337c61e3bf6ac3df24f89c7981b55e95f4b759
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2021-04-06 19:23:43 -0300

    Conditionally use defaultLocale

--- i18n.js
+++ i18n.js
@@ -26,7 +26,7 @@ if (process.env.FRONTEND_FQDN_EN && process.env.FRONTEND_FQDN_FR) {
 
 module.exports = {
   locales: ['en', 'fr'],
-  defaultLocale: 'en',
+  ...(!domains && { defaultLocale: 'en' }),
   domains,
   pages: {
     '*': ['common', 'layouts', 'menu'],
