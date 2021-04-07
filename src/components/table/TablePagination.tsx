26cb6d91de3484dde10676f4a012d6e6db48f33d
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2021-04-07 11:23:14 -0300

    Table pagination tweak

--- src/components/table/TablePagination.tsx
+++ src/components/table/TablePagination.tsx
@@ -20,47 +20,50 @@ export const TablePagination = ({ className, page }: TablePaginationProps): JSX.
   const { pathname, query } = useRouter();
   const { t } = useTranslation();
 
+  const showPrevious = page.number > 1;
+  const showNext = page.number < page.totalPages;
+
   return (
-    <div className={`tw-bg-white tw-px-4 tw-py-3 tw-flex tw-items-center tw-justify-between tw-border-t tw-border-gray-200 ${className}`}>
-      <div className="tw-hidden sm:tw-block">
-        <div className="tw-text-sm tw-text-gray-700 tw-hidden sm:tw-block">
-          <Trans
-            i18nKey="common:pagination.info"
-            components={{ label: <span className="tw-font-bold" /> }}
-            values={{
-              start: (page.number - 1) * page.size + 1,
-              end: page.number === page.totalPages ? page.totalElements : page.number * page.size,
-              total: page.totalElements,
-            }}
-          />
+    <div className={`tw-bg-white tw-px-4 tw-py-3 tw-flex tw-flex-col sm:tw-flex-row sm:tw-items-center tw-space-y-2 sm:tw-space-y-0 tw-justify-between tw-border-t tw-border-gray-200 ${className}`}>
+      <div className="tw-text-sm tw-text-gray-700">
+        <Trans
+          i18nKey="common:pagination.info"
+          components={{ label: <span className="tw-font-bold" /> }}
+          values={{
+            start: (page.number - 1) * page.size + 1,
+            end: page.number === page.totalPages ? page.totalElements : page.number * page.size,
+            total: page.totalElements,
+          }}
+        />
+      </div>
+      {(showPrevious || showNext) && (
+        <div className="tw-flex tw-items-center tw-justify-between tw-space-x-4">
+          {showPrevious && (
+            <Link
+              href={{
+                pathname,
+                query: { ...query, page: page.number - 1 },
+              }}
+              passHref>
+              <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
+                {t('common:pagination.previous')}
+              </a>
+            </Link>
+          )}
+          {showNext && (
+            <Link
+              href={{
+                pathname,
+                query: { ...query, page: page.number + 1 },
+              }}
+              passHref>
+              <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
+                {t('common:pagination.next')}
+              </a>
+            </Link>
+          )}
         </div>
-      </div>
-      <div className="tw-flex tw-items-center tw-justify-between tw-space-x-4">
-        {page.number > 1 && (
-          <Link
-            href={{
-              pathname,
-              query: { ...query, page: page.number - 1 },
-            }}
-            passHref>
-            <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
-              {t('common:pagination.previous')}
-            </a>
-          </Link>
-        )}
-        {page.number < page.totalPages && (
-          <Link
-            href={{
-              pathname,
-              query: { ...query, page: page.number + 1 },
-            }}
-            passHref>
-            <a className="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border tw-border-gray-300 tw-text-sm tw-font-bold tw-no-underline hover:tw-no-underline focus:tw-no-underline tw-rounded-md tw-text-gray-700 visited:tw-text-gray-700 tw-bg-white hover:tw-text-gray-500 focus:tw-text-gray-500">
-              {t('common:pagination.next')}
-            </a>
-          </Link>
-        )}
-      </div>
+      )}
     </div>
   );
 };
